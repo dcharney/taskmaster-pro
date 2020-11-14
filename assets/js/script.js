b825b9418ab56c56d59e6fb6da9cd9a6c99fa962
delaney <delaneycharney@gmail.com>
2020-11-14 17:25:00 -0500

    drag and drop and delete feature works

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,49 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    update: function(event) {
+        var tempArr = []
+
+        // loop over current set of children in sortable list
+        $(this).children().each(function() {
+            var text = $(this)
+                .find("p")
+                .text()
+                .trim();
+
+            var date = $(this)
+                .find("span")
+                .text()
+                .trim();
+            
+            tempArr.push({
+                text: text,
+                date: date
+            });
+        });
+
+        var arrName = $(this)
+            .attr("id")
+            .replace("list-","");
+
+        tasks[arrName] = tempArr;
+        saveTasks();
+    }
+});
+
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+        ui.draggable.remove();
+    }
+});
+
 // load tasks for the first time
 loadTasks();
 
