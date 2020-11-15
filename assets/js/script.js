4f84cdffad3c166c96e4d1f418f6aedf0391c717
delaney <delaneycharney@gmail.com>
2020-11-14 20:38:52 -0500

    added date feature and task priority highlighting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,37 @@
 var tasks = {};
 
+var auditTask = function(taskE1) {
+    var date = $(taskE1).find("span").text().trim();
+
+    var time = moment(date, "L").set("hour", 17);
+
+    $(taskE1).removeClass("list-group-item-warning list-group-item-danger");
+
+    if (moment().isAfter(time)) {
+        $(taskEl).addClass("list-group-item-danger");
+    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(taskE1).addClass("list-group-item-warning");
+    }
+};
+
 var createTask = function(taskText, taskDate, taskList) {
-  // create elements that make up a task item
-  var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+    // create elements that make up a task item
+    var taskLi = $("<li>").addClass("list-group-item");
+    var taskSpan = $("<span>")
+        .addClass("badge badge-primary badge-pill")
+        .text(taskDate);
+    var taskP = $("<p>")
+        .addClass("m-1")
+        .text(taskText);
 
-  // append span and p element to parent li
-  taskLi.append(taskSpan, taskP);
+    // append span and p element to parent li
+    taskLi.append(taskSpan, taskP);
 
+    // check due date
+    auditTask(taskLi);
 
-  // append to ul list on the page
-  $("#list-" + taskList).append(taskLi);
+    // append to ul list on the page
+    $("#list-" + taskList).append(taskLi);
 };
 
 var loadTasks = function() {
@@ -24,10 +40,10 @@ var loadTasks = function() {
   // if nothing in localStorage, create a new object to track all task status arrays
   if (!tasks) {
     tasks = {
-      toDo: [],
-      inProgress: [],
-      inReview: [],
-      done: []
+        toDo: [],
+        inProgress: [],
+        inReview: [],
+        done: []
     };
   }
 
@@ -87,12 +103,20 @@ $(".list-group").on("click", "span", function() {
   
     // swap out elements
     $(this).replaceWith(dateInput);
+
+    // enable jquery ui datepicker
+    dateInput.datepicker({
+        minDate: 1,
+        onClose: function() {
+            $(this).trigger("change");
+        }
+    });
   
     // automatically focus on new element
     dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
     // get current text
     var date = $(this).val().trim();
 
@@ -110,6 +134,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
     // replace textarea with p element
     $(this).replaceWith(taskSpan);
+
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -198,7 +224,9 @@ $("#trash").droppable({
     }
 });
 
+$("#modalDueDate").datepicker({
+    minDate: 1
+});
+
 // load tasks for the first time
 loadTasks();
-
-
